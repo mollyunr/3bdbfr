7b45d4bcb0337480018332f4e3b59a6b97c7170b
spgibb <sgibb@nevada.unr.edu>
2016-04-01 21:46:41 -0700

    Create create_database.py

---
+++ create_database.py
@@ -0,0 +1,54 @@
+import sys
+import datetime
+import time
+import os
+
+#
+# Takes database name as argument
+#
+
+arguments = len(sys.argv)
+
+action = 'create_database'
+log = 'false'
+t = time.time()
+timestamp = datetime.datetime.fromtimestamp(t).strftime('%Y-%m-%d %H:%M:%S')
+
+f = open('userdatabase.txt', 'r')
+fileLength = sum(1 for line in f)
+f.close()
+
+if fileLength > 0 and arguments > 1:
+    f = open('userdatabase.txt', 'r')
+    temp = list(f)
+    f.close()
+
+    for index in range(0, fileLength):
+        temp[index] = temp[index].split();
+
+    if not os.path.exists(sys.argv[1]):
+        os.mkdir(sys.argv[1])
+        log = 'true'
+
+    for index in range(0, fileLength):
+        if not os.path.exists(sys.argv[1] + '/' + temp[index][0]):
+            os.mkdir(sys.argv[1] + '/' + temp[index][0])
+
+    path, dirs, files = os.walk(sys.argv[1]).next()
+    directory_count = len(dirs)
+    database_size = os.path.getsize(sys.argv[1])
+
+    path, dirs, files = os.walk(sys.argv[1] + '/' + temp[0][0]).next()
+    picture_count = len(files)
+
+    f = open(sys.argv[1] + '/' + 'database_information.txt', 'w')
+    f.write('users: ' + str(directory_count) + '\n')
+    f.write('database size: ' + str(database_size) + ' bytes' + '\n')
+    f.write('pictures per user: ' + str(picture_count) + '\n')
+    f.close()
+
+    if log == 'true':
+        f = open('log.txt', 'a')
+        f.write(timestamp + '\n')
+        f.write(action + '\n' + '\n')
+        f.close()
