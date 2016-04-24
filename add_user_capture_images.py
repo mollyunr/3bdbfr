2c54645f640189016a92b938fd517e5eaeca23c8
bhosang <bhosang@nevada.unr.edu>
2016-04-24 15:33:28 -0700

    Added files via upload

--- add_user_capture_images.py
+++ add_user_capture_images.py
@@ -20,34 +20,38 @@ print "Using camera %s" % cameras[0]
 cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],(100,100))
 
 #Make directory
-if not os.path.exists(user):
-    os.makedirs(user)
+if os.path.exists(database_name):
+	directory = database_name + "/" + user + "/"
+	if not os.path.exists(directory):
+	    os.makedirs(directory)
 
-raw_input("Waiting for button click...")
-cam.start()
+	raw_input("Waiting for button click...")
+	cam.start()
 
-image = cam.get_image()
-pygame.image.save(image, user+"/photo1.png")
-time.sleep(0.5)
+	image = cam.get_image()
+	pygame.image.save(image, directory+"/photo1.png")
+	time.sleep(0.5)
 
-image = cam.get_image()
-pygame.image.save(image, user+"/photo2.png")
-time.sleep(0.5)
+	image = cam.get_image()
+	pygame.image.save(image, directory+"/photo2.png")
+	time.sleep(0.5)
 
-image = cam.get_image()
-pygame.image.save(image, user+"/photo3.png")
-time.sleep(0.5)
+	image = cam.get_image()
+	pygame.image.save(image, directory+"/photo3.png")
+	time.sleep(0.5)
 
-image = cam.get_image()
-pygame.image.save(image, user+"/photo4.png")
-time.sleep(0.5)
+	image = cam.get_image()
+	pygame.image.save(image, directory+"/photo4.png")
+	time.sleep(0.5)
 
-image = cam.get_image()
-pygame.image.save(image, user+"/photo5.png")
-time.sleep(0.5)
+	image = cam.get_image()
+	pygame.image.save(image, directory+"/photo5.png")
+	time.sleep(0.5)
 
-if( image != 0 ):
-    print "Picture taken"
-if( image == 0 ):
-    print "Picture NOT taken"
-pygame.camera.quit()
+	if( image != 0 ):
+	    print "Picture taken"
+	if( image == 0 ):
+	    print "Picture NOT taken"
+	pygame.camera.quit()
+else:
+	print "Database does not exist"
