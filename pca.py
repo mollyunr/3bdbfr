b2b28d8162b2d83491013598cfaa284d7ed967a1
spgibb <sgibb@nevada.unr.edu>
2016-04-30 16:57:05 -0700

    Update pca.py

--- pca.py
+++ pca.py
@@ -23,7 +23,7 @@ if arguments > 1:
     # load all training images into list with unsigned integer (8 bit) values
     trainingFaces = []
     for index in range(0, len(trainingFileNames)):
-        f = open(trainingFileNames[index], "rb")
+        f = open(trainingFileNames[index], 'rb')
         garbage = f.readline().split()
         image = np.fromfile(f,dtype = np.uint8)
         trainingFaces.append(image)
@@ -33,22 +33,22 @@ if arguments > 1:
     trainingFaces = np.array(trainingFaces)
 
     # train PCA model with 95% detail retained, using training faces
-    pca = decomposition.PCA(n_components = 0.80, copy = 'true')
+    pca = decomposition.PCA(n_components = 0.95, copy = 'true')
     pca.fit(trainingFaces)
 
     # get eigenfaces for both testing and training data
     trainingEigenfaces = pca.transform(trainingFaces)
 
-    np.save("components_", pca.components_)
+    np.save('components_', pca.components_)
 
-    np.save("explained_variance_", pca.explained_variance_)
+    np.save('explained_variance_', pca.explained_variance_)
 
-    np.save("mean_", pca.mean_)
+    np.save('mean_', pca.mean_)
 
-    f = open("n_components__noise_variance.txt", "w")
+    f = open('n_components__noise_variance.txt', 'w')
     f.write(str(pca.n_components_))
-    f.write("\n")
+    f.write('\n')
     f.write(str(pca.noise_variance_))
     f.close()
 
-    np.save("trainingEigenfaces", trainingEigenfaces)
+    np.save('trainingEigenfaces', trainingEigenfaces)
