a8aabe57e5e8df040ab1a08d3cd36649133b082b
Molly Headland <mollyh@nevada.unr.edu>
2016-05-02 22:22:45 -0700

    tabs to spaces

--- camera.py
+++ camera.py
@@ -8,53 +8,53 @@ import StringIO
 import shutil
 from select import select
 
-SIZE = (500,500)
+SIZE = (100,100)
 CAM_INDEX = 1
 
 def timer(timeout):
-	rlist, _, _ = select([sys.stdin], [], [], timeout)
-	if rlist:
-	    s = sys.stdin.readline()
-	    return s
+    rlist, _, _ = select([sys.stdin], [], [], timeout)
+    if rlist:
+        s = sys.stdin.readline()
+        return s
 
 def initializeCamera(index):
-	pygame.camera.init()
-	cam = 0
-	#use default webcam
-	try:
-		cam = pygame.camera.Camera(pygame.camera.list_cameras()[index],SIZE)
+    pygame.camera.init()
+    cam = 0
+    #use default webcam
+    try:
+        cam = pygame.camera.Camera(pygame.camera.list_cameras()[index],SIZE)
 	
-	except IndexError:
-		cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],SIZE)
+    except IndexError:
+        cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],SIZE)
 
-	#check if webcam detected, if not report error message
-	return cam
+    #check if webcam detected, if not report error message
+    return cam
 
 def initializeScreen(cam):
-	img = cam.get_image()
-	width = img.get_width()
-	height = img.get_height()
-	screen = pygame.display.set_mode((width,height))
-	return screen
+    img = cam.get_image()
+    width = img.get_width()
+    height = img.get_height()
+    screen = pygame.display.set_mode((width,height))
+    return screen
 
 def takePictures(cam, directory):
-	count = 0 
-	while count < 12:
-		delay = timer(1)
-		img = cam.get_image()		
-		pygame.image.save(img, directory+"/"+str(count)+".png")
-		count += 1
+    count = 0 
+    while count < 1:
+        delay = timer(1)
+        img = cam.get_image()		
+        pygame.image.save(img, directory+"/"+str(count)+".png")
+        count += 1
 
 def savePictures(directory):
-	camera = initializeCamera(CAM_INDEX)
-	camera.start()
+    camera = initializeCamera(CAM_INDEX)
+    camera.start()
 
-	user_directory = directory
-	if not os.path.exists(user_directory):
-		os.mkdir(user_directory)
+    user_directory = directory
+    if not os.path.exists(user_directory):
+        os.mkdir(user_directory)
 
-	takePictures(camera, user_directory)
-	camera.stop()
+    takePictures(camera, user_directory)
+    camera.stop()
 
 if __name__ == '__main__':
     savePictures("test_db")
